7e30bb0675df6e23e5e1389dc460e29c1194c70f
user1-prajwal <ewitewit451@gmail.com>
2026-06-15 13:00:20 +0530

    fix: Case-sensitive import

--- collab-editor-frontend/src/App.jsx
+++ collab-editor-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
+import Landing from './pages/landing'
 import EditorPage from './pages/EditorPage'
 
 function App() {
