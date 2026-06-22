e7dd2a450eeee7bd324dfcb896ffb14f57a76466
user1-prajwal <ewitewit451@gmail.com>
2026-06-22 16:30:04 +0530

    refactor: move language configs to dedicated constants file

--- collab-editor-frontend/src/constants.js
+++ collab-editor-frontend/src/constants.js
@@ -1,4 +1,22 @@
-export const LANGUAGES = ['javascript', 'python', 'cpp', 'java', 'typescript']
+export const LANGUAGES = [
+  'python',
+  'c',
+  'cpp',
+  'java',
+  'typescript'
+]
+
+export const LANGUAGE_DISPLAY = {
+  // javascript: 'JavaScript',
+  python: 'Python',
+  c: 'C',
+  cpp: 'C++',
+  java: 'Java',
+  typescript: 'TypeScript'
+}
+
+
+
 export const COLORS = ['#FF6B6B', '#4ECDC4', '#42F527', '#ffffff', '#fc15fc']
 export const SESSION_COLOR = COLORS[Math.floor(Math.random() * COLORS.length)]
 export const BACKEND_URL = 'https://collab-editor-backend-p7at.onrender.com'
