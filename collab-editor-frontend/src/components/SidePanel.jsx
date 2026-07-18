7bcc28ae358cce9bd6c487b52cd967392eb09f00
user1-prajwal <ewitewit451@gmail.com>
2026-07-18 21:08:14 +0530

    go language icon  added at the begin of file name

--- collab-editor-frontend/src/components/SidePanel.jsx
+++ collab-editor-frontend/src/components/SidePanel.jsx
@@ -12,7 +12,7 @@ const LANG_ICONS = {
   php:        '/lang_icons/php.png',
   ruby:       '/lang_icons/Ruby.png',
   haskell:    '/lang_icons/Haskell.png',
-  go:         '/lang_icons/go.png',
+  go:         '/lang_icons/golang.png',
   rust:       '/lang_icons/rust.png',
   plaintext:  '/lang_icons/text.png',
 }
